e1b1323b891a8c48a1af74508a11f2c5e2460c35
Juliana Ribeiro <jumaju10@hotmail.com>
2020-07-08 20:08:49 -0300

    ServerList adicionado -add

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import ServerButton from '../ServerButton';
+import { Container, Separator } from './styles';
+
+const ServerList: React.FC = () => {
+    return (
+        <Container>
+            <ServerButton isHome/>
+
+            <Separator/>
+
+            <ServerButton/>
+            <ServerButton hasNotifications/>
+            <ServerButton mentions={3}/>
+            <ServerButton/>
+            <ServerButton/>
+            <ServerButton/>
+            <ServerButton/>
+            <ServerButton/>
+            <ServerButton mentions={5}/>
+            <ServerButton/>
+            <ServerButton hasNotifications/>
+        </Container>
+    );
+};
+
+export default ServerList;
