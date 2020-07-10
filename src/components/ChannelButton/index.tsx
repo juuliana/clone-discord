1b304d77e7d634ad64ce0605c2cc8937c23059f2
Juliana Ribeiro <jumaju10@hotmail.com>
2020-07-09 23:30:30 -0300

    ChannelButton atualizado

--- src/components/ChannelButton/index.tsx
+++ src/components/ChannelButton/index.tsx
@@ -2,28 +2,25 @@ import React from 'react';
 
 import { Container, HashtagIcon, InviteIcon, SettingsIcon } from './styles';
 
-export interface Props{
-    channelName: string;
-    selected?: boolean;
+export interface Props {
+  channelName: string;
+  selected?: boolean;
 }
 
-const ChannelButton: React.FC<Props> = ({
-    channelName,
-    selected
-}) => {
-    return (
-        <Container className={selected ? 'active' : ''}>
-            <div>
-                <HashtagIcon/>
-                <span>{channelName}</span>
-            </div>
+const ChannelButton: React.FC<Props> = ({ channelName, selected }) => {
+  return (
+    <Container className={selected ? 'active' : ''}>
+      <div>
+        <HashtagIcon />
+        <span>{channelName}</span>
+      </div>
 
-            <div>
-                <InviteIcon/>
-                <SettingsIcon/>
-            </div>
-        </Container>
-    );
+      <div>
+        <InviteIcon />
+        <SettingsIcon />
+      </div>
+    </Container>
+  );
 };
 
 export default ChannelButton;
